249aa25aee82a52df6fc26587958b62a08cc8224
Andrija Jovanovic <46319817+andrijaJ01@users.noreply.github.com>
2022-10-16 14:51:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt update
+apt install wget git -y
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | apt-key add -
+apt update
+apt install box64 -y
+echo "done, now you should be able to play"
+echo "there are two variations with snd without dfhack"
