a3b4cf5fbeebc69cb5ad177033c7d8277462bacd
Andrija Jovanovic <46319817+andrijaJ01@users.noreply.github.com>
2022-10-16 14:55:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ apt install wget git -y
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | apt-key add -
 apt update
 apt install box64 -y
+echo "installing libs"
+apt install libsdl1.2debian libgtk2.0-0 libsdl-image1.2 libglu1-mesa libsdl-ttf2.0-0 libopenal-dev libsndfile1-dev libncursesw5
+clear
 echo "done, now you should be able to play"
 echo "there are two variations with and without dfhack"
